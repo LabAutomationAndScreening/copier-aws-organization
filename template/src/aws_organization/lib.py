f507feb2fd696f7a8846a37ca3a1e141979ed15a
Eli Fine <ejfine@gmail.com>
2025-02-04 19:31:40 +0000

    more lib

--- template/src/aws_organization/lib.py
+++ template/src/aws_organization/lib.py
@@ -3,22 +3,20 @@ from pydantic import Field
 
 
 class AwsAccountInfo(BaseModel, frozen=True):
-    account_id: str
-    account_name: str
-
-
-class AwsLogicalWorkload(BaseModel, frozen=True):
+    version: str = "0.0.1"
+    id: str
     name: str
-    production_accounts: list[AwsAccountInfo] = Field(
+
+
+class AwsLogicalWorkload(BaseModel):
+    version: str = "0.0.1"
+    name: str
+    prod_accounts: list[AwsAccountInfo] = Field(
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
     staging_accounts: list[AwsAccountInfo] = Field(
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
-    development_accounts: list[AwsAccountInfo] = Field(
+    dev_accounts: list[AwsAccountInfo] = Field(
         default_factory=list
     )  # TODO: convert to a set with deterministic ordering to avoid false positive diffs
-
-
-class AwsLogicalWorkloads(BaseModel, frozen=True):
-    logical_workloads: list[AwsLogicalWorkload]
