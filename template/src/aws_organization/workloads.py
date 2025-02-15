601f372dd66b5f771457ee0146478db64fbca94e
Eli Fine <eli88fine@gmail.com>
2025-02-15 12:56:10 -0500

    Refactor to simplify (#4)
    ## Why is this change necessary?
    The program.py file was getting enormous
    
    
     ## How does this change address the issue?
    Breaks out most of the actions taking place in program.py to separate
    modules.
    Creates a single create_workloads function where specific workloads can
    be put.
    
    
     ## What side effects does this change have?
    Easier to read
    
    
     ## How is this change tested?
    Deploying to my AWS organization and seeing no diff

---
+++ template/src/aws_organization/workloads.py
@@ -0,0 +1,7 @@
+from .lib import CommonWorkloadKwargs
+from .lib import OrganizationalUnits
+
+
+def create_workloads(*, org_units: OrganizationalUnits, common_workload_kwargs: CommonWorkloadKwargs):
+    # Create workloads here
+    pass
